e325097d04ed568bdadf90cc368c97984fa7fbef
fairilyuwaldi <fairilf803@gmail.com>
2026-04-07 10:26:52 +0800

    PKL SMKN 5 Bantaeng

---
+++ aidil/lirik.py
@@ -0,0 +1,21 @@
+import time
+start_time = time.time()
+
+print("kau bermasalah jiwa, akupun rasa gila ")
+print("jodoh akal-akalan neraka kita bersama")
+
+print("kau langganan menangis, lakimu muntah-muntah")
+# ini adalah comment
+a = 10
+print ("begitu terus sampai iblis tobat dan sedekah")
+print("terkadang rasanya leher terbakar hingga pagi")
+print("seperti aku hidup berpasangan dengan api")
+print("berhenti, ulangi, psikolog, dan terapi aku isi bensin, kita coba lagi")
+print("tapi sebelumnya")
+print("sejuta sayang untukmu, cinta")
+print("karna akupun bola panas juga")
+print(time.time() - start_time, "detik")
+# ke kita bisa mengcompile python 
+# yang namanya bytecode 
+# cara mengcompile, buka terminal dan tuliskan
+# python -m py_compile lirik.py
