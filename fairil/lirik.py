e8efe322cbeaea0d56f972884600a7ab4da9ebc5
fairilyuwaldi <fairilf803@gmail.com>
2026-04-07 12:39:17 +0800

    PKL SMKN 5 Bantaeng

---
+++ fairil/lirik.py
@@ -0,0 +1,16 @@
+import time
+start_time = time.time()
+
+print("Bintang bintang dan pepohonan")
+print("Kedua bersujud pada-Nya")
+print("Masih kukagum tentang langit")
+print("Kokoh dan tinggi tak bertiang")
+print("Siapakah aku sebenarnya")
+# ini adalah comment
+a = 10 # ini adalah comment juga
+print (a)
+print(time.time() - start_time, "detik")
+# kita bisa mengcompile python ke 
+# yang namanya bytecode
+# cara mengcompile, buka terminal dan tuliskan 
+# python -m py_compile lirik.py
---
+++ nur/lirik.py
@@ -0,0 +1,15 @@
+import time
+start_time = time.time()
+print("Terakhir ku masih lebih kecil darimu")
+print("Sekarang bisa ku lihat atas kepalamu")
+print("Yang ternyata memutih")
+print("Sudah hampir semua rambut")
+print("Seingatku tak sampai sebanyak itu")
+# ini adalah comment
+a = 10 
+print (a)
+print(time.time() - start_time, "detik")
+# kita bisa mengcompile python ke
+# yang namanya bytecode 
+# cara mengcompile, buka terminal dan tuliskan
+# python -m py_compile lirik.py
