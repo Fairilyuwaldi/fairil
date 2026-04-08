53ef6f9c3cd726795d6812dafb01b008a5f267e8
fairilyuwaldi <fairilf803@gmail.com>
2026-04-08 11:43:39 +0800

    PKL SMKN 5 Bantaeng

---
+++ aidil/test2.py
@@ -0,0 +1,26 @@
+# Variable adalah tempat untuk menyimpan data
+
+# menaru / assigment nilai
+a = 10
+x = 5 
+panjang = 1000
+
+# pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# pebamaan
+nilai_y = 15 # dengan menggunakan underscore
+juta10 = 100000000 # ini boleh
+nilaiZ = 17.5 # ini boleh
+
+# pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+#assignment indirect
+b = a
+print("Nilai b = ", b)
+
