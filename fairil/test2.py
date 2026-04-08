e44ab6663a9f69fa4e84868d0ceffd7c0078773d
fairilyuwaldi <fairilf803@gmail.com>
2026-04-08 15:45:30 +0800

    PKL SMKN 5 Bantaeng

--- fairil/test2.py
+++ fairil/test2.py
@@ -1,25 +1,37 @@
-# Variabel adalah tempat menyimpan data 
+# a =, 10, a adalah dengan nilai 10
 
-# menaru / assignment nilai 
-a = 10
-x = 5
-panjang = 1000
+# tipe data: Angka satuan yang gak ada
+ # koma nya (integer)
+data_integer = 1
+print("data : ", type (data_integer))
+print("- bertipe : ", type (data_integer))
 
-# pemanggilan pertama 
-print('Nilai a =  ',a)
-print("Nilai x =  ",x)
-print("Nilai panjang = ",panjang)
+# tipe data: angka dengan koma (float
+data_float = 1,5
+print("data : ", data_float)
+print("- bertipe : ", type (data_float))
 
-# penamaan 
-nilai_y = 15 # dengan mengggunakan underscore
-juta10 = 1000000 # ini boleh 
-nilaiZ = 17.5 # ini boleh 
+# tipe data: kumpulan karakter (string)
+data_string = "fairil"
+print("data : ", data_float)
+print("- bertipe : ", type (data_float))
 
-# pemanggilan kedua 
-print("Nilai a ", a )
-a = 7
-print("Nilai a = ", a )
+# tipe data: biner true/false (boolean)
+data_bool = True
+print("data : ", data_float)
+print("- bertipe : ", type (data_float))
 
-# assignment indirect
-b = a 
-print("Nilai a = ", b)
+## tipe data khusus
+
+# bilangan kompleks
+data_complex = complex(5,6)
+print("data : ", data_complex)
+print("- bertipe : ", type (data_complex))
+
+# tipe data dari bahasa C
+
+from ctypes import c_double
+
+data_c_double = c_double(10,5)
+print("data : ", data_c_double)
+print("-bertipe ", type(data_c_double))
