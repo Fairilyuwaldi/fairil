b4c390127a28db41116cb91acdfe1ca0773a380f
fairilyuwaldi <fairilf803@gmail.com>
2026-04-14 10:58:34 +0800

    PKL SMKN 5 Bantaeng

---
+++ aidil/pekan_kedua/test8.py
@@ -0,0 +1,10 @@
+# ELIF = else if statment
+
+nama = input("Nama kamyu siapa? ")
+
+if nama == "ucup": # kondisi 1
+    print("Hai gantengg beuds") # aksi true 1
+elif nama == "otong": # kondisi 2
+    print("Hai si kece bangtttss!!")
+
+print("ini adalah akhir dari program")
---
+++ fairil/test7.py
@@ -0,0 +1,10 @@
+# ELIF = if statement)
+
+nama = input("Nama kamu siapa? ")
+
+if nama == "ucup": # kondisi 1
+    print("Hai ganteeeng beuds!") aksi true 1
+elif nama == "otong": # kondisi 2
+    print("Hai si kece bangeeets!!") # aksi true 2
+
+    print("ini adalah akhir dari program")
