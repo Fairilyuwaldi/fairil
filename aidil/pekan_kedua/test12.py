212ab74ba5fb7e278d0b2748015a97130d5c2c6a
fairilyuwaldi <fairilf803@gmail.com>
2026-04-16 09:15:17 +0800

    PKL SMKN 5 Bantaeng

---
+++ aidil/pekan_kedua/test12.py
@@ -0,0 +1,69 @@
+# Latihan perulangan segitiga
+
+sisi = 4
+
+# 1. menggunakan For
+
+# dummy variable
+print("awal For")
+count = 1
+for i in range(4)(sisi):
+    print("*")
+    count += 1
+
+print("akhir dari for")
+# 2. menggunakan while
+
+print("awal while")
+count = 1
+while True:
+    print("*"*count)
+    count += 1
+    
+    if count > sisi:
+        break
+
+print("akhir while")
+
+# 3. hanya ganjil saja
+
+print("awal while")
+count = 1
+while True:
+    if ~(count%2):
+        # print jika ganjil
+        print("*"*count)
+        count += 1
+    else:
+        # akan kembali ke atas jika ganjil
+        count += 1
+        continue
+
+    # akan break jika count melebihi sisi
+    if count > sisi:
+        break
+
+print("akhir while")
+
+# 4. hanya ganjil saja
+
+print("awal while")
+count = 1
+spasi = int(sisi/2)
+
+while True:
+    if (count%2):
+        # print jika ganjil
+        print(" "*spasi,"+"*count)
+        spasi -= 1
+        count += 1
+    else:
+        # akan kembali ke atas jika ganjil
+        count += 1
+        continue
+
+    # akan break jika count melebihi sisi
+    if count > sisi:
+        break
+
+    print("akhir while")
