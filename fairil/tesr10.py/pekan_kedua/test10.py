ecdfcaf3291dbb91547ce3e66fd8401aad2734d2
fairilyuwaldi <fairilf803@gmail.com>
2026-04-16 10:03:53 +0800

    PKL SMKN 5 Bantaeng

--- fairil/tesr10.py/pekan_kedua/test10.py
+++ fairil/tesr10.py/pekan_kedua/test10.py
@@ -1,10 +1,10 @@
 # perulangan (loop)
 
 # for kondisi:
-# aksi
+#   aksi
 
 # ini dengan list
-angka2_list = [0,1,2,8,10,] # ini adalah list
+angka2_list = [0,2,4,8,10] # ini adalah list
 print(angka2_list)
 
 for i in angka2_list:
@@ -12,7 +12,7 @@ for i in angka2_list:
 
 print("akhir dari program 1 \n")
 
-# ini dengan range 
+# ini dengan range
 angka2_range = range(5)
 
 for i in angka2_range:
@@ -23,16 +23,16 @@ print("akhir dari program 2 \n")
 angka2_range = range(1,10)
 
 for i in angka2_range:
-    print(f"i sekarang _> {i}")
-    # print ("saya keren")
+    print(f"i sekarang -> {i}")
+    # print("saya keren")
 
 print("akhir dari program 3 \n")
 
 # menggunakan string
 
-data_str = "saya ganteng abiees"
+data_str = "saya ganteng abis"
 
-for huruf in data_str: 
+for huruf in data_str:
     print(huruf)
 
 print("akhir dari program 4 \n")
