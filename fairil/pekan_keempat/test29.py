4d79a44758da3cf01913cba877c1cb07c81f0ed5
fairilyuwaldi <fairilf803@gmail.com>
2026-04-27 11:00:19 +0800

    PKL SMKN 5 Bantaeng

---
+++ fairil/pekan_keempat/test29.py
@@ -0,0 +1,18 @@
+''''Membuat fungsi'''
+
+def hello_world():
+    '''fungsi menampilkan hello'''
+    print("hello world")
+    print("Kepada Ucup Serucup")
+    print("Dan juga kepada Otong Surotong")
+
+    hello_world()
+    hello_world()
+
+# fungsi ()
+
+def fungsi():
+    '''pemanggilan fungsi harus seelah di buat'''
+    print("ini adalah fungsi")
+
+fungsi()
