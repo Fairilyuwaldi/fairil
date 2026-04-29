4d0ec2633bd1c0bf9352ebbeaf9a64f29099812d
fairilyuwaldi <fairilf803@gmail.com>
2026-04-29 10:05:06 +0800

    PKL SMKN 5 Bantaeng

---
+++ fairil/pekan_keempat/test30.py
@@ -0,0 +1,34 @@
+''' Fungsi dengan argumen (input) '''
+
+# Template
+# def nama_fungsi(argument):
+#       Badan fungsi
+
+
+def hello_world(nama):
+    '''fyngsi hello world menerima input dengan variable'''
+    print(f"selamat datang dunia wahai {nama}")
+
+
+hello_world("ucup")
+hello_world("aseppp")
+
+# program tambah
+
+def tambah(angka_1,angka_2):
+    '''fungsi tambah'''
+    hasil = angka_1 + angka_2
+    print(f"{angka_1} + {angka_2} = {hasil}")
+
+tambah(1,5)
+tambah(10000,1)
+
+def say_hi(list_peserta):
+    '''fungsi say hi'''
+    data_peserta = list_peserta.copy()
+    for peserta in data_peserta:
+        print(f"yang terhormat {peserta}")
+
+anggota_boyband = ["ucup","otong","dudung"]
+
+say_hi(anggota_boyband)
